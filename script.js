d884631b428f978ae00ce35c3675a62afcd5d9ff
Gusty0666 <gusty0666@outlook.com>
2026-04-23 18:53:03 +0800

    修复了加减乘除因界面设置偶有失灵的bug

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
 
-function calculate() {
+function calculateResult() {
     try {
         // eval 是一个简单的计算函数
         display.value = eval(display.value);
